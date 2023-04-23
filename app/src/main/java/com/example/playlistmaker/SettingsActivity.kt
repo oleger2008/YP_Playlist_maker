938b8f40c13458bb7cf14fc16b4680a37e608a76
oleger2008 <otsoy1995@yandex.ru>
2023-04-23 20:07:29 +0300

    delete unused import

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -3,7 +3,6 @@ package com.example.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 
 class SettingsActivity : AppCompatActivity() {
