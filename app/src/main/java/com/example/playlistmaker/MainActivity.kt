99982fe3253d523f7b84ada29a8be4c02d1350fe
oleger2008 <otsoy1995@yandex.ru>
2023-04-23 19:03:42 +0300

    add mock reaction on main frame buttons clicks

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        searchButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity, "click on 'Search' button", Toast.LENGTH_SHORT).show()
+        }
+
+        val mediaLibraryButton = findViewById<Button>(R.id.media_library_button)
+        mediaLibraryButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity, "click on 'Media library' button", Toast.LENGTH_SHORT).show()
+        }
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity, "click on 'Settings' button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
