78a8c0be7342f48dbab068601d6b0cbe374a8b2d
oleger2008 <otsoy1995@yandex.ru>
2023-05-08 17:52:01 +0300

    add some buttons reactions

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,34 @@ class SettingsActivity : AppCompatActivity() {
         returnButton.setOnClickListener {
             val mainIntent = Intent(this, MainActivity::class.java)
             startActivity(mainIntent)
+        }
+
+        val shareButton = findViewById<LinearLayout>(R.id.share_settings_line)
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, R.string.share_link)
+            }
+            startActivity(shareIntent)
+        }
+
+        val supportButton = findViewById<LinearLayout>(R.id.support_settings_line)
+        supportButton.setOnClickListener {
+            val supIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, getString(R.string.support_email))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_letter_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.support_letter_text))
+            }
+            startActivity(supIntent)
+        }
+
+        val licenseAgreementButton = findViewById<LinearLayout>(R.id.license_agreement_settings_line)
+        licenseAgreementButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(getString(R.string.license_agreement_link))
+            }
+            startActivity(intent)
         }
     }
 }
