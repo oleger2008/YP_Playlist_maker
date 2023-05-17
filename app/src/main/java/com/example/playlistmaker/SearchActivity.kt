c73c1f5e59c45b6263cbc8ddbbc7436c7dd868e0
oleger2008 <otsoy1995@yandex.ru>
2023-05-18 02:18:51 +0300

    add enum SearchResponseStatus

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -16,6 +16,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+    enum class SearchResponseStatus {
+        OK, EMPTY, ERROR
+    }
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
