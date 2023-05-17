4e3431e7d1f3a120252583dda8f419f956f441e3
oleger2008 <otsoy1995@yandex.ru>
2023-05-18 02:44:47 +0300

    fix trackDuration.text binding

--- app/src/main/java/com/example/playlistmaker/TrackListViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackListViewHolder.kt
@@ -7,6 +7,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class TrackListViewHolder(parent: ViewGroup) :
     RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(
@@ -26,6 +28,7 @@ class TrackListViewHolder(parent: ViewGroup) :
             .into(trackImage)
         trackName.text = model.trackName
         trackAuthor.text = model.artistName
-        trackDuration.text = model.trackTime
+        trackDuration.text = SimpleDateFormat("mm:ss", Locale.getDefault())
+            .format(model.trackTimeMillis)
     }
 }
