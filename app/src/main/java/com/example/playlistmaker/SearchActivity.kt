4ac5157698834f6f0970981588ba9eecd4f4502e
oleger2008 <otsoy1995@yandex.ru>
2023-05-24 01:33:03 +0300

    make SearchResponseStatus private

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -21,7 +21,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
-    enum class SearchResponseStatus {
+    private enum class SearchResponseStatus {
         OK, EMPTY, ERROR
     }
 
