60bc774c1a4e02af905d0c823f2c9b190a7a0332
oleger2008 <otsoy1995@yandex.ru>
2023-06-04 01:52:51 +0300

    add field trackId

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -3,6 +3,7 @@ package com.example.playlistmaker
 import com.google.gson.annotations.SerializedName
 
 data class Track(
+    val trackId: Int,
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: String,
