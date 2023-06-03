128ffa4bbe256622436e74c7ea4ee6d692845c68
oleger2008 <otsoy1995@yandex.ru>
2023-06-03 20:19:03 +0300

    edit App.kt
    separated sharedPreferences as lateinit var
    add method isDarkThemeDefault
    add sharedPreferences edit

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -1,16 +1,19 @@
 package com.example.playlistmaker
 
 import android.app.Application
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatDelegate
+import android.content.res.Configuration
 
 class App : Application() {
     var darkTheme = false
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate() {
         super.onCreate()
 
-        val sharedPreferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
-        darkTheme = sharedPreferences.getBoolean(DARK_THEME, false)
+        sharedPreferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+        darkTheme = sharedPreferences.getBoolean(DARK_THEME, isDarkThemeDefault())
         switchTheme(darkTheme)
     }
 
@@ -23,5 +26,13 @@ class App : Application() {
                 AppCompatDelegate.MODE_NIGHT_NO
             }
         )
+        sharedPreferences.edit()
+            .putBoolean(DARK_THEME, darkTheme)
+            .apply()
+    }
+
+    private fun isDarkThemeDefault(): Boolean {
+        return resources.configuration.uiMode
+            .and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
     }
 }
