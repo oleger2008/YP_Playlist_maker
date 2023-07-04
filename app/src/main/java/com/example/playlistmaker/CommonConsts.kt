6cc7d0af8d9b940cee5ed82dda5c67e297e12223
oleger2008 <otsoy1995@yandex.ru>
2023-07-04 19:13:17 +0300

    rename const 'PLAYERS_DATA' to 'PLAYER_TRACK_DATA'

--- app/src/main/java/com/example/playlistmaker/CommonConsts.kt
+++ app/src/main/java/com/example/playlistmaker/CommonConsts.kt
@@ -1,5 +1,5 @@
 package com.example.playlistmaker
 
 const val APP_PREFERENCES = "APP_PREFERENCES"
-const val PLAYERS_DATA = "PLAYERS_DATA"
+const val PLAYER_TRACK_DATA = "PLAYER_TRACK_DATA"
 const val DARK_THEME = "DARK_THEME"
