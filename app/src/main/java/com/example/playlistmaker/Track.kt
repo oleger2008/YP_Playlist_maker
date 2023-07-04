e37a6e58585d57118c1054e9e84966c1b282b928
oleger2008 <otsoy1995@yandex.ru>
2023-07-04 03:08:45 +0300

    add missed fields for Track

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -7,5 +7,9 @@ data class Track(
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: String,
-    val artworkUrl100: String
+    val artworkUrl100: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String
 ) {}
